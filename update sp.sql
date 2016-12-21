65ad074b290197cc1e38d31d0215728b4f6ed3a7
xambinx <xu.idris.maulana@gmail.com>
2016-12-21 14:26:42 +0700

    tambah fix sp product view ga ada supplier price

--- update sp.sql
+++ update sp.sql
@@ -22,3 +22,54 @@ BEGIN
     
 
 END
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_pr_viewproductlist`(IN `product_id` INT, IN `page_no` INT, IN `row_ct` INT, IN `isactive` BIT)
+BEGIN
+declare start_no int;
+declare end_no int;
+
+if(page_no<1) then set page_no=1;end if;
+set start_no = ((page_no-1)*row_ct);
+
+if row_ct>0 and product_id=0
+then
+	SELECT pr.`product_id`,
+    pr.`merk_id`,
+    mr.merk_nama,
+    pr.`satuan_id`,
+    st.satuan_nama,
+    pr.`product_nama`,
+    pr.`remarks`,
+    pr.`stock`,
+    pr.`min_stock`,
+	  pr.`isactive`,
+	  pr.`default_price`,
+      pr.supplier_price
+	FROM `product` pr
+    INNER JOIN merk mr ON mr.merk_id=pr.merk_id
+    INNER JOIN satuan st ON st.satuan_id=pr.satuan_id
+
+	WHERE ((pr.isactive=1 AND isactive=1) OR (isactive=0))
+	AND (pr.product_id=product_id or product_id=0 OR product_id is null)
+	LIMIT start_no,row_ct;
+else
+	SELECT pr.`product_id`,
+    pr.`merk_id`,
+    mr.merk_nama,
+    pr.`satuan_id`,
+    st.satuan_nama,
+    pr.`product_nama`,
+    pr.`remarks`,
+	  pr.`stock`,
+    pr.`min_stock`,
+	  pr.`isactive`,
+	  pr.`default_price`,
+      pr.supplier_price
+	FROM `product` pr
+    INNER JOIN merk mr ON mr.merk_id=pr.merk_id
+    INNER JOIN satuan st ON st.satuan_id=pr.satuan_id
+
+	WHERE ((pr.isactive=1 AND isactive=1) OR (isactive=0))
+	AND (pr.product_id=product_id or product_id=0 OR product_id is null);
+end if;
+END
